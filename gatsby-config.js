080fc248e92af44061364eb4fe96deab6a7e7607
captainsloths <alex.eiselstein@gmail.com>
2021-02-25 17:14:27 -0600

    dev build

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,18 @@
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        display: `standalone`,
+        icon: `src/images/favicon.ico`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
